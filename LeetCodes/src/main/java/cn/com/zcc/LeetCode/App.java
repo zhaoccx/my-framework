f6aac2d94c73a5cf8e66db67e2325b7472373ec1
趙常春 <zhaoccx@hotmail.com>
2019-07-09 14:25:30 +0800

    init

--- LeetCodes/src/main/java/cn/com/zcc/LeetCode/App.java
+++ LeetCodes/src/main/java/cn/com/zcc/LeetCode/App.java
@@ -2,12 +2,9 @@ package cn.com.zcc.LeetCode;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
