8e55707ccdba921151a55511eca3226b0d99c176
趙常春 <zhaoccx@hotmail.com>
2019-07-17 18:18:17 +0800

    test set

--- LeetCodes/src/main/java/cn/com/zcc/LeetCode/App.java
+++ LeetCodes/src/main/java/cn/com/zcc/LeetCode/App.java
@@ -1,10 +1,21 @@
 package cn.com.zcc.LeetCode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Hello world!
  */
 public class App {
+    // TODO OK.
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Set<Short> set = new HashSet<>();
+        for (short i = 0; i < 100; i++) {
+            set.add(i);
+            set.remove(i - 1);
+        }
+        System.out.println(set.size());
     }
 }
